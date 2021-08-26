40e893d2fd1c725216d19c09b1c9faf481011e52
Angelito Salud <asalud@you-source.com>
2021-08-26 12:25:46 +0800

    changed image name

--- test/core/cog.ts
+++ test/core/cog.ts
@@ -25,7 +25,7 @@
 //   it('should return expected cog metadata', (done) => {
 //     const version: string = JSON.parse(fs.readFileSync('package.json').toString('utf8')).version;
 //     cogUnderTest.getManifest(null, (err, manifest: CogManifest) => {
-//       expect(manifest.getName()).to.equal('automatoninc/outreach');
+//       expect(manifest.getName()).to.equal('automatoninc/_outreach');
 //       expect(manifest.getLabel()).to.equal('Outreach');
 //       expect(manifest.getVersion()).to.equal(version);
 //       done();
