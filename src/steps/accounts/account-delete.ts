85f3234b1de54c382625a2a195e86f11d731e603
Angelito Salud <asalud@you-source.com>
2021-11-01 12:45:34 +0800

    added id as identifier

--- src/steps/accounts/account-delete.ts
+++ src/steps/accounts/account-delete.ts
@@ -5,7 +5,7 @@ import { Step, RunStepResponse, FieldDefinition, StepDefinition, RecordDefinitio
 export class AccountDeleteStep extends BaseStep implements StepInterface {
 
   protected stepName: string = 'Delete an Outreach Account';
-  protected stepExpression: string = 'delete the outreach account with (?<idField>[a-zA-Z0-9_]+) (?<identifier>[a-zA-Z0-9_]+)';
+  protected stepExpression: string = 'delete the outreach account with (?<idField>[a-zA-Z0-9_]+) (?<identifier>.+)';
   protected stepType: StepDefinition.Type = StepDefinition.Type.ACTION;
   protected expectedFields: Field[] = [{
     field: 'idField',
@@ -33,7 +33,14 @@ export class AccountDeleteStep extends BaseStep implements StepInterface {
     const identifier = stepData.identifier;
 
     try {
-      const accounts = await this.client.getAccountsByIdentifier(idField, identifier);
+      let accounts = [];
+      if (idField === 'id') {
+        const idAccount = await this.client.getAccountById(identifier);
+        accounts.push(idAccount);
+      } else {
+        accounts = await this.client.getAccountsByIdentifier(idField, identifier);
+      }
+      
       if (accounts.length === 0) {
         // If the client does not return an account, return an error.
         return this.fail('No Account was found with %s %s', [idField, identifier]);
