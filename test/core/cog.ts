b6562ab1500cf8f4ef4588c74f52f5094099b512
Russell Hart <russell.hart@stackmoxie.com>
2024-09-16 10:00:14 -0400

    publish to acr

--- test/core/cog.ts
+++ test/core/cog.ts
@@ -26,7 +26,7 @@ describe('Cog:GetManifest', () => {
   it('should return expected cog metadata', (done) => {
     const version: string = JSON.parse(fs.readFileSync('package.json').toString('utf8')).version;
     cogUnderTest.getManifest(null, (err, manifest: CogManifest) => {
-      expect(manifest.getName()).to.equal('automatoninc/outreach');
+      expect(manifest.getName()).to.equal('stackmoxie/outreach');
       expect(manifest.getLabel()).to.equal('Outreach');
       expect(manifest.getVersion()).to.equal(version);
       done();
